e17b67ef292f0388dfd3a58fb9dd9ec83a95fee1
Niloy-Khan30 <kamrul.hassan.niloy@g.bracu.ac.bd>
2025-08-17 14:16:59 +0600

    search btn added

--- index.php
+++ index.php
@@ -17,10 +17,102 @@
         <p><span>ALL</span> <span class="description-title">MOVIES</span></p>
       </div><!-- End Section Title -->
 
+      <!-- searching movie  -->
+      <form action="index.php" method="post">
+        <div class="row" style = 'margin-left: 100px; margin-bottom : 50px'>
+
+          <!-- Movie search input -->
+          <div class="col-lg-3 col-md-6 d-flex">
+            <div class="form-group">
+              <input type="text" class="form-control" name="movie_search" placeholder="Search Movie">
+            </div>
+          </div>
+
+          <!-- Category dropdown -->
+          <div class="col-lg-3 col-md-6 d-flex"  style = 'margin-left: -150px;'>
+            <div class="form-group">
+              <select name="catID" class="form-control">
+                <option value="">Select Category</option>
+                <?php
+                $sql = "SELECT * FROM catagories"; // use your real table name
+                $result = mysqli_query($conn, $sql);
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_array($result)){
+                        echo "<option value='".$row['catID']."'>".$row['catname']."</option>";
+                    }
+                } else {
+                    echo "<option value=''>No categories found</option>";
+                }
+                ?>
+              </select>
+            </div>
+          </div>
+
+          <!-- Search button -->
+          <div class="col-lg-3 col-md-6 d-flex" style = 'margin-left: -200px;'>
+            <div class="form-group">
+              <button type="submit" class="btn btn-primary" name = 'searchbtn'>Search</button>
+            </div>
+          </div>
+
+        </div>
+    </form>
+
+    <script>
+        function validateSearch() {
+          let category = document.getElementById("catID").value;
+          if (category === "") {
+            alert("⚠ Please select a category before searching!");
+            return false; // prevent form submission
+          }
+          return true; // allow form to submit
+        }
+    </script>
+
+
+
       <div class="container">
 
         <div class="row gy-4">
-          <?php $sql = "SELECT movies.*, catagories.catname FROM movies JOIN catagories ON movies.catID = catagories.catID";
+          <?php 
+            if(isset($_POST['searchbtn'])){
+              $movie_name = $_POST['movie_search'];
+              $catid = $_POST['catID'];
+
+              $sql = "SELECT movies.*, catagories.catname FROM movies JOIN catagories ON movies.catID = catagories.catID where movies.title like '%$movie_name%' and movies.catID = $catid";
+              $result = mysqli_query($conn, $sql);
+              if(mysqli_num_rows($result) > 0){
+              while($row = mysqli_fetch_array($result)){
+
+
+                ?>
+                  <div class="col-lg-3 col-md-6 d-flex align-items-stretch aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                    <div class="team-member">
+                      <div class="member-img">
+                        <img src="admin/uploads/<?= $row['image']?>" style ='height : 230px; width : 200px' alt="">
+                        <div class="social">
+                          <a href="booking.php"  style = 'width : 100% ; color : black;'>BOOK NOW</a>
+                        </div>
+                      </div>
+                      <div class="member-info">
+                        <h4><?= $row['title']?></h4>
+                        <span><?= $row['catname']?></span>
+                      </div>
+                    </div>
+                  </div>
+                <?php
+              }
+              }
+
+          
+
+
+            }else{
+
+            
+      
+          
+          $sql = "SELECT movies.*, catagories.catname FROM movies JOIN catagories ON movies.catID = catagories.catID";
           $result = mysqli_query($conn, $sql);
           if(mysqli_num_rows($result) > 0){
           while($row = mysqli_fetch_array($result)){
@@ -45,7 +137,7 @@
         <?php
           }
         }
-
+      }
           ?>
 
         </div>
@@ -55,6 +147,8 @@
       </div>
 
     </section>
+
+
 
 
 
